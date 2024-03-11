f1a606494a91df323880faf68a757ae9ca3144d5
Lucas Gelenski <gelenskilucas@gmail.com>
2024-03-11 09:00:27 -0300

    Adicionando verificações

--- src/script.js
+++ src/script.js
@@ -5,7 +5,7 @@ const cpfInput = document.querySelector("#cpf");
 const emailInput = document.querySelector("#email");
 const passwordInput = document.querySelector("#password");
 const jobSelect = document.querySelector("#job");
-const messageTextarea = document.querySelector("#message");
+const messageInput = document.querySelector("#message");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -13,9 +13,51 @@ form.addEventListener("submit", (event) => {
     alert("Por favor, preencha seu nome");
     return;
   }
+
+  if (cpfInput.value === "" || !validateCpf(cpfInput.value)) {
+    alert("Por favor, preencha seu cpf");
+    return;
+  }
+
+  if (emailInput.value === "" || !isEmailValid(emailInput.value)) {
+    alert("Por favor, preencha seu e-mail");
+    return;
+  }
+
+  if (!validatePassword(passwordInput.value, 8)) {
+    alert("Por favor, preencha sua senha corretamente");
+    return;
+  }
+
+  if (messageInput.value === "") {
+    alert("Por favor, preencha com a sua mensagem");
+  }
+  form.submit();
 });
 
-if (cpfInput.value === "" || !isCPFValid(cpfInput.value)) {
-  alert("Por favor, preencha seu cpf");
-  return;
+//Padrão -- usuario12@servico.com
+function isEmailValid(email) {
+  const emailRegex = new RegExp(/^[a-zA-Z0-9]+@[a-zA-Z0-9]+.[a-zA-Z]{2,0}/);
+  if (emailRegex.test(email)) {
+    return true;
+  }
+  return false;
+}
+
+function validatePassword(password, minDigits) {
+  if (password.length >= minDigits) {
+    return true;
+  }
+  return false;
+}
+
+//Padrão -- xxx.xxx.xxx--xx
+function validateCpf(cpf) {
+  const cpfRegex = new RegExp(
+    /^([0-9]){3}\.([0-9]){3}\.([0-9]){3}-([0-9]){2}$/
+  );
+  if (cpfRegex.test(cpf)) {
+    return true;
+  }
+  return false;
 }
